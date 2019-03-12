170e254ee82123792d91e8ea28c493811f37573f
romkatv <roman.perepelitsa@gmail.com>
2019-03-12 09:38:21 +0100

    buffer keyboard input before benchmarking

--- README.md
+++ README.md
@@ -37,36 +37,39 @@ plugins=(
 # Usage
 
 Type `zsh-prompt-benchmark` in an interactive zsh shell and press and hold
-`[ENTER]` until you see benchmark results. The output also includes your prompt with
-all non-ascii characters escaped. This is to enable you to easily share the results
-with others (e.g., if you want to complain to your zsh theme provider about
-high prompt latency). Not only will they see how fast (or how slow!) your prompt
-renders but also what it actually looks like.
+`[ENTER]` until you see benchmark results. It'll take 15 seconds with default arguments. The output
+also includes your prompt with all non-ascii characters escaped. This is to enable you to easily
+share the results with others (e.g., if you want to complain to your zsh theme provider about high
+prompt latency). Not only will they see how fast (or how slow!) your prompt renders but also what it
+actually looks like.
 
 You can pass two optional arguments to `zsh-prompt-benchmark`:
 
   * `$1` -- Benchmark for this many seconds. Default is 5.
-  * `$2` -- Skip this many first prompts and only then start measuring. Default is 5.
+  * `$2` -- Sleep for this many seconds before benchmarking to fill the keyboard input buffer.
+            This mitigates the problem caused by slow key repeat rate (see below). Default is 10.
 
-Make sure your repeat key rate is high enough that your shell is unable to keep up. If
-you see empty lines between prompts or if prompts keep being printed after you release
-`[ENTER]`, your repeat key rate is sufficient.
+Make sure your repeat key rate is high enough that your shell is unable to keep up. While not
+benchmarking, press and hold `[ENTER]`. If you see empty lines between prompts or if prompts keep
+being printed after you release `[ENTER]`, your repeat key rate is sufficient. If it's not,
+you can artificially boost it by buffering keyboard input buffer. Your effective key repeat
+rate is multiplied by `1 + $2 / $1`. With default settings this is `1 + 10 / 5` == `3`.
 
 Example output:
 
 ```
 ************************************************************
-                Prompt Benchmark Results                    
+                Prompt Benchmark Results
 ************************************************************
-Warmup prompts (skipped)  100
-Benchmarked prompts       382
-Total time                5.012s
-Time per prompt           13.1ms
+Warmup duration           10s
+Benchmarked prompts       1737
+Total time                5.002s
+Time per prompt           2.88ms
 ************************************************************
 
-PROMPT=$'\342'$'\225'$'\255'$'\342'$'\224'$'\200'%f%b%k%K\{001\}\ %F\{226\}$'\357'$'\200'$'\243'%F\{226\}\ %K\{004\}%F\{001\}$'\356'$'\202'$'\260'\ %F\{000\}$'\357'$'\201'$'\274'\ %F\{000\}\~/testrepo\ %K\{003\}%F\{004\}$'\356'$'\202'$'\260'\ %F\{000\}$'\357'$'\204'$'\223'\ $'\357'$'\204'$'\246'\ master\ $'\357'$'\201'$'\225'\ $'\357'$'\201'$'\252'\ $'\357'$'\201'$'\231'\ $'\357'$'\200'$'\233'\ 2\ $'\357'$'\200'$'\232'\ 1\ $'\357'$'\200'$'\234'\ 2\ %k%F\{003\}$'\356'$'\202'$'\260'%f\ $'\n'$'\342'$'\225'$'\260'$'\342'$'\224'$'\200'\ 
+PROMPT=$'\342'$'\225'$'\255'$'\342'$'\224'$'\200'%f%b%k%K\{004\}\ %F\{000\}$'\357'$'\201'$'\274'\ %F\{000\}\~/.oh-my-zsh/custom/themes/powerlevel10k\ %K\{002\}%F\{004\}$'\356'$'\202'$'\260'\ %F\{000\}$'\357'$'\204'$'\223'\ $'\357'$'\204'$'\246'\ master\ %k%F\{002\}$'\356'$'\202'$'\260'%f\ $'\n'$'\342'$'\225'$'\260'$'\342'$'\224'$'\200'\ 
 
-RPROMPT=%\{$'\033'\[1A%\}%f%b%k%F\{102\}$'\356'$'\202'$'\262'%K\{102\}\ %F\{002\}%F\{002\}$'\357'$'\200'$'\214'\ %F\{214\}$'\356'$'\202'$'\262'%K\{214\}\ %F\{000\}2\ %F\{000\}$'\357'$'\200'$'\223'\ %F\{005\}$'\356'$'\202'$'\262'%K\{005\}\ %F\{000\}%D\{%H:%M:%S\}\ %F\{000\}$'\357'$'\200'$'\227'\ %F\{004\}$'\356'$'\202'$'\262'%K\{004\}\ %F\{000\}hello\ world\ %F\{000\}$'\357'$'\200'$'\205'\ %E%\{$'\033'\[00m%\}%\{$'\033'\[1B%\}
+RPROMPT=%\{$'\033'\[1A%\}%f%b%k%F\{102\}$'\356'$'\202'$'\262'%K\{102\}\ %F\{002\}%F\{002\}$'\357'$'\200'$'\214'\ %F\{005\}$'\356'$'\202'$'\262'%K\{005\}\ %F\{000\}%D\{%H:%M:%S\}\ %F\{000\}$'\357'$'\200'$'\227'\ %E%\{$'\033'\[00m%\}%\{$'\033'\[1B%\}
 
 Tip: To print one of the reported prompts, execute the
 following command with ${P} replaced by the prompt string.
@@ -76,7 +79,7 @@ following command with ${P} replaced by the prompt string.
 For example, here's how you can print the same left prompt
 (PROMPT) that was benchmarked:
 
-  print -lP BEGIN $'\342'$'\225'$'\255'$'\342'$'\224'$'\200'%f%b%k%K\{001\}\ %F\{226\}$'\357'$'\200'$'\243'%F\{226\}\ %K\{004\}%F\{001\}$'\356'$'\202'$'\260'\ %F\{000\}$'\357'$'\201'$'\274'\ %F\{000\}\~/testrepo\ %K\{003\}%F\{004\}$'\356'$'\202'$'\260'\ %F\{000\}$'\357'$'\204'$'\223'\ $'\357'$'\204'$'\246'\ master\ $'\357'$'\201'$'\225'\ $'\357'$'\201'$'\252'\ $'\357'$'\201'$'\231'\ $'\357'$'\200'$'\233'\ 2\ $'\357'$'\200'$'\232'\ 1\ $'\357'$'\200'$'\234'\ 2\ %k%F\{003\}$'\356'$'\202'$'\260'%f\ $'\n'$'\342'$'\225'$'\260'$'\342'$'\224'$'\200'\  END
+  print -lP BEGIN $'\342'$'\225'$'\255'$'\342'$'\224'$'\200'%f%b%k%K\{004\}\ %F\{000\}$'\357'$'\201'$'\274'\ %F\{000\}\~/.oh-my-zsh/custom/themes/powerlevel10k\ %K\{002\}%F\{004\}$'\356'$'\202'$'\260'\ %F\{000\}$'\357'$'\204'$'\223'\ $'\357'$'\204'$'\246'\ master\ %k%F\{002\}$'\356'$'\202'$'\260'%f\ $'\n'$'\342'$'\225'$'\260'$'\342'$'\224'$'\200'\  END
 ************************************************************
 ```
 
--- zsh-prompt-benchmark.plugin.zsh
+++ zsh-prompt-benchmark.plugin.zsh
@@ -16,67 +16,76 @@
 # if you press and hold [ENTER], how many prompts will be printed per second?
 #
 #   $1 -- Benchmark for this many seconds. Default is 5.
-#   $2 -- Skip this many first prompts and only then start measuring. Default is 5.
+#   $2 -- Sleep for this many seconds before benchmarking to fill the keyboard input buffer.
+#         This mitigates the problem caused by slow key repeat rate (see below). Default is 10.
 #
-# After calling this function and press and hold [ENTER] until you see benchmark results.
-# The output also includes your prompt with all non-ascii characters escaped. This
-# is to enable you to easily share the results with others (e.g., if you want
-# to complain to your zsh theme provider about high prompt latency). Not only will they
-# see how fast (or how slow!) your prompt renders but also what it actually looks like.
+# After calling this function in an interactive shell you need to press and hold [ENTER] until
+# you see benchmark results. It'll take 15 seconds with default arguments. The output also includes
+# your prompt with all non-ascii characters escaped. This is to enable you to easily share the
+# results with others (e.g., if you want to complain to your zsh theme provider about high prompt
+# latency). Not only will they see how fast (or how slow!) your prompt renders but also what it
+# actually looks like.
+#
+# Make sure your repeat key rate is high enough that your shell is unable to keep up. While not
+# benchmarking, press and hold [ENTER]. If you see empty lines between prompts or if prompts keep
+# being printed after you release [ENTER], your repeat key rate is sufficient. If it's not,
+# you can artificially boost it by buffering keyboard input buffer. Your effective key repeat
+# rate is multiplied by 1 + $2 / $1. With default settings this is 1 + 10 / 5 == 3.
 function zsh-prompt-benchmark() {
   typeset -gHF3 _BENCHMARK_PROMPT_DURATION=${1:-5}
-  typeset -gHi _BENCHMARK_PROMPT_NUM_SKIP=${2:-5}
-  typeset -gHi _BENCHMARK_PROMPT_SAMPLE_IDX=0
+  typeset -gHi  _BENCHMARK_PROMPT_WARMUP_DURATION=${2:-10}
+  typeset -gHi  _BENCHMARK_PROMPT_SAMPLE_IDX=0
+  typeset -gHF3 _BENCHMARK_PROMPT_START_TIME=0
   >&2 echo "Enabling prompt benchmarking for ${_BENCHMARK_PROMPT_DURATION}s" \
-           "after skipping ${_BENCHMARK_PROMPT_NUM_SKIP} prompt(s)."
+           "after buffering keyboard input for ${_BENCHMARK_PROMPT_WARMUP_DURATION}s."
   >&2 echo "Press and hold [ENTER] until you see benchmark results."
   add-zsh-hook precmd _zsh_prompt_benchmark_precmd
 
   typeset -gHf _zsh_prompt_benchmark_precmd() {
-    if ((_BENCHMARK_PROMPT_SAMPLE_IDX == _BENCHMARK_PROMPT_NUM_SKIP)); then
-      typeset -gHF _BENCHMARK_PROMPT_START_TIME=$EPOCHREALTIME
-    elif ((_BENCHMARK_PROMPT_SAMPLE_IDX > _BENCHMARK_PROMPT_NUM_SKIP)); then
-      local -F NOW=$EPOCHREALTIME
-      if ((NOW >= _BENCHMARK_PROMPT_START_TIME + _BENCHMARK_PROMPT_DURATION)); then
-        local -i N=$((_BENCHMARK_PROMPT_SAMPLE_IDX - _BENCHMARK_PROMPT_NUM_SKIP))
-        local -F3 T=$((NOW - _BENCHMARK_PROMPT_START_TIME))
-        local -F1 P=$((1000 * T / N))
-        local LP=$(eval LC_ALL=C printf '%q' \"$PROMPT\")
-        local RP=$(eval LC_ALL=C printf '%q' \"$RPROMPT\")
-        >&2 echo -E "************************************************************"
-        >&2 echo -E "                Prompt Benchmark Results                    "
-        >&2 echo -E "************************************************************"
-        >&2 echo -E "Warmup prompts (skipped)  $_BENCHMARK_PROMPT_NUM_SKIP"
-        >&2 echo -E "Benchmarked prompts       $N"
-        >&2 echo -E "Total time                ${T}s"
-        >&2 echo -E "Time per prompt           ${P}ms"
-        >&2 echo -E "************************************************************"
-        >&2 echo -E ""
-        >&2 echo -E "PROMPT=$LP"
-        >&2 echo -E ""
-        >&2 echo -E "RPROMPT=$RP"
-        >&2 echo -E ""
-        >&2 echo -E "Tip: To print one of the reported prompts, execute the"
-        >&2 echo -E "following command with \${P} replaced by the prompt string."
-        >&2 echo -E ""
-        >&2 echo -E "  print -lP BEGIN \${P} '' END"
-        >&2 echo -E ""
-        >&2 echo -E "For example, here's how you can print the same left prompt"
-        >&2 echo -E "(PROMPT) that was benchmarked:"
-        >&2 echo -E ""
-        >&2 echo -E "  print -lP BEGIN $LP END"
-        >&2 echo -E "************************************************************"
-        >&2 echo -E ""
-        >&2 echo -E "Press 'q' to continue..."
-        local _
-        IFS='' read -rsd q _
-        add-zsh-hook -D precmd _zsh_prompt_benchmark_precmd
-        unset -m "_BENCHMARK_PROMPT_*"
-        unset -f _zsh_prompt_benchmark_precmd
-        return
-      fi
-    fi
+    local -F now=$EPOCHREALTIME
     ((++_BENCHMARK_PROMPT_SAMPLE_IDX))
+    if ((now < _BENCHMARK_PROMPT_START_TIME + _BENCHMARK_PROMPT_DURATION)); then
+      return
+    fi
+    if (( _BENCHMARK_PROMPT_START_TIME )); then
+      local -i N=$((_BENCHMARK_PROMPT_SAMPLE_IDX - 1))
+      local -F3 T=$((now - _BENCHMARK_PROMPT_START_TIME))
+      local -F2 P=$((1000 * T / N))
+      local LP=$(eval LC_ALL=C printf '%q' \"$PROMPT\")
+      local RP=$(eval LC_ALL=C printf '%q' \"$RPROMPT\")
+      >&2 echo -E "************************************************************"
+      >&2 echo -E "                Prompt Benchmark Results                    "
+      >&2 echo -E "************************************************************"
+      >&2 echo -E "Warmup duration           ${_BENCHMARK_PROMPT_WARMUP_DURATION}s"
+      >&2 echo -E "Benchmarked prompts       $N"
+      >&2 echo -E "Total time                ${T}s"
+      >&2 echo -E "Time per prompt           ${P}ms"
+      >&2 echo -E "************************************************************"
+      >&2 echo -E ""
+      >&2 echo -E "PROMPT=$LP"
+      >&2 echo -E ""
+      >&2 echo -E "RPROMPT=$RP"
+      >&2 echo -E ""
+      >&2 echo -E "Tip: To print one of the reported prompts, execute the"
+      >&2 echo -E "following command with \${P} replaced by the prompt string."
+      >&2 echo -E ""
+      >&2 echo -E "  print -lP BEGIN \${P} '' END"
+      >&2 echo -E ""
+      >&2 echo -E "For example, here's how you can print the same left prompt"
+      >&2 echo -E "(PROMPT) that was benchmarked:"
+      >&2 echo -E ""
+      >&2 echo -E "  print -lP BEGIN $LP END"
+      >&2 echo -E "************************************************************"
+      >&2 echo -E ""
+      >&2 echo -E "Press 'q' to continue..."
+      unset -m "_BENCHMARK_PROMPT_*"
+      unset -f _zsh_prompt_benchmark_precmd
+      add-zsh-hook -D precmd _zsh_prompt_benchmark_precmd
+      local _ && IFS='' read -rsd q _
+    else
+      sleep $_BENCHMARK_PROMPT_WARMUP_DURATION
+      typeset -gHF _BENCHMARK_PROMPT_START_TIME=$EPOCHREALTIME
+    fi
   }
 }
 
