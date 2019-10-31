c636fe15f444ddfbdf647a98f4e9c5db21e4a0b0
romkatv <roman.perepelitsa@gmail.com>
2019-10-31 16:46:50 +0100

    add an optional argument that gets passed through eval after the benchmark

--- zsh-prompt-benchmark.plugin.zsh
+++ zsh-prompt-benchmark.plugin.zsh
@@ -18,6 +18,7 @@
 #   $1 -- Benchmark for this many seconds. Default is 2.
 #   $2 -- Sleep for this many seconds before benchmarking to fill the keyboard input buffer.
 #         This mitigates the problem caused by slow key repeat rate (see below). Default is 8.
+#   $3 -- Run this (via `eval`) when done. Default is empty.
 #
 # After calling this function in an interactive shell you need to press and hold [ENTER] until
 # you see benchmark results. It'll take 10 seconds with default arguments.
@@ -28,40 +29,48 @@
 # you can artificially boost it by buffering keyboard input buffer. Your effective key repeat
 # rate is multiplied by 1 + $2 / $1. With default settings this is 1 + 8 / 2 == 5.
 function zsh-prompt-benchmark() {
-  typeset -gF3 _benchmark_prompt_duration=${1:-2}
-  typeset -gi  _benchmark_prompt_warmup_duration=${2:-8}
-  typeset -gi  _benchmark_prompt_sample_idx=0
-  typeset -gF3 _benchmark_prompt_start_time=0
-  >&2 echo "Enabling prompt benchmarking for ${_benchmark_prompt_duration}s" \
-           "after buffering keyboard input for ${_benchmark_prompt_warmup_duration}s."
-  >&2 echo "Press and hold [ENTER] until you see benchmark results."
+  emulate -L zsh
+
   zmodload zsh/datetime
   autoload -Uz add-zsh-hook
   add-zsh-hook precmd _zsh_prompt_benchmark_precmd
+
+  typeset -gF3 _benchmark_prompt_duration=${1:-2}
+  typeset -gi  _benchmark_prompt_warmup_duration=${2:-8}
+  typeset -g   _benchmark_prompt_on_done=${3:-}
+  typeset -gi  _benchmark_prompt_sample_idx=0
+  typeset -gF3 _benchmark_prompt_start_time=0
+
+  >&2 echo "Enabling prompt benchmarking for ${_benchmark_prompt_duration}s" \
+           "after buffering keyboard input for ${_benchmark_prompt_warmup_duration}s."
+  >&2 echo "Press and hold [ENTER] until you see benchmark results."
 
   function _zsh_prompt_benchmark_precmd() {
     local -F now=EPOCHREALTIME
     ((++_benchmark_prompt_sample_idx))
     ((now >= _benchmark_prompt_start_time + _benchmark_prompt_duration)) || return
     (( _benchmark_prompt_start_time )) && {
+      emulate -L zsh
+      local REPLY on_done=$_benchmark_prompt_on_done
       local -i n=$((_benchmark_prompt_sample_idx - 1))
       local -F3 t=$((now - _benchmark_prompt_start_time))
       local -F2 p=$((1000 * t / n))
-      >&2 echo -E "********************************************************************"
-      >&2 echo -E "                      Prompt Benchmark Results                      "
-      >&2 echo -E "********************************************************************"
-      >&2 echo -E "Warmup duration      ${_benchmark_prompt_warmup_duration}s"
-      >&2 echo -E "Benchmark duration   ${t}s"
-      >&2 echo -E "Benchmarked prompts  $n"
-      >&2 echo -E "Time per prompt      ${p}ms  <-- prompt latency (lower is better)"
-      >&2 echo -E "********************************************************************"
-      >&2 echo -E ""
-      >&2 echo -E "Press 'q' to continue..."
+      >&2 print -lr --                                                         \
+        "********************************************************************" \
+        "                      Prompt Benchmark Results                      " \
+        "********************************************************************" \
+        "Warmup duration      ${_benchmark_prompt_warmup_duration}s"           \
+        "Benchmark duration   ${t}s"                                           \
+        "Benchmarked prompts  $n"                                              \
+        "Time per prompt      ${p}ms  <-- prompt latency (lower is better)"    \
+        "********************************************************************" \
+        ""                                                                     \
+        "Press 'q' to continue..."
       unset -m "_benchmark_prompt_*"
-      unset -f _zsh_prompt_benchmark_precmd
       add-zsh-hook -D precmd _zsh_prompt_benchmark_precmd
-      local r
-      IFS='' read -rsd q r || true
+      unset -f _zsh_prompt_benchmark_precmd
+      IFS='' read -rsd q
+      eval $on_done || true
     } || {
       sleep $_benchmark_prompt_warmup_duration
       typeset -gF _benchmark_prompt_start_time=EPOCHREALTIME
